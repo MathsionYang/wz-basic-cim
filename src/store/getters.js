e1b4cd159cdbd586fa6fb3f29fec6af0e606fc3a
xwy0210 <58430568+xwy0210@users.noreply.github.com>
2021-01-30 14:46:34 +0800

    Delete src directory

--- src/store/getters.js
+++
@@ -1,4 +0,0 @@
-// 获取state
-export const userInfo = state => state.userInfo
-export const resize = state => state.resize
-
--- src/store/modules/login/getters.js
+++
@@ -1,3 +0,0 @@
-// 获取state
-export const username = state => state.username
-export const pwd = state => state.pwd
